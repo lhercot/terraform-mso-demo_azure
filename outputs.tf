411e6d62d2d874023390d76e17224a082fe2e4cb
Lionel Hercot <lhercot@cisco.com>
2020-10-05 21:30:42 -0700

    Add site id to outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "subnet_dn" {
   value = "uni/tn-${var.tenant}/ctxprofile-${var.name_prefix}Hybrid_Cloud_VRF-westus/cidr-[10.101.210.0/24]/subnet-[10.101.210.128/25]"
   description = "The ACI subnet object DN"
 }
+
+output "azure_site_id" {
+  value = data.mso_site.azure.id
+  description = "The id of the Azure Site"
+}
