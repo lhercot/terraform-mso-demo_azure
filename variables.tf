4d8e4785a326c1da124c0311d3892368b23d8f49
Lionel Hercot <lhercot@cisco.com>
2020-10-05 21:09:37 -0700

    Add missing tenant variable

--- variables.tf
+++ variables.tf
@@ -4,6 +4,12 @@ variable "name_prefix" {
   description = "A prefix that will be added to all the object names"
 }
 
+variable "tenant" {
+  type = string
+  default = "WoS"
+  description = "The MSO tenant to use for this configuration"
+}
+
 variable "site_name" {
   type = string
   default = "Azure-West"
